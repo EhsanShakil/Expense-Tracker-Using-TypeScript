d64038145d0ab4a1dd4e68034d4db9f346b9f278
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-08-28 23:40:25 +0500

    Updating Add Transaction Component

--- src/components/Add_Transaction.tsx
+++ src/components/Add_Transaction.tsx
@@ -4,6 +4,22 @@ const AddTransaction = () => {
   return (
     <div>
       <h1>Add Transaction</h1>
+      <form>
+        <label htmlFor="description">Description</label>
+        <input
+          type="text"
+          id="description"
+          placeholder="Enter Tranaction Description"
+        />
+        <label htmlFor="amount">Transaction Amount</label>
+        <input
+          type="number"
+          id="amount"
+          placeholder="Enter Tranaction Amount"
+        />
+      </form>
     </div>
   );
 };
+
+export default AddTransaction;
