855709e8f3d8704fbb1e8496fcc6726c2289a4f5
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-09-01 23:16:00 +0500

    Updating Balance Component

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import "./App.css";
 import { GlobalProvider } from "./GlobalState/Gloabal_State";
 import AddTransaction from "./components/Add_Transaction";
 import TransactionHistory from "./components/Transaction_History";
+import Balance from "./components/Balance";
 
 function App() {
   return (
     <GlobalProvider>
       <h1>Expense Tracker</h1>
+      <Balance />
       <AddTransaction />
       <TransactionHistory />
     </GlobalProvider>
--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -1,1 +1,22 @@
-import react from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../GlobalState/Gloabal_State";
+
+const Balance = () => {
+  const { transaction }: any = useContext(GlobalContext);
+
+  const amount = transaction.map((transaction: any) => transaction.Amount);
+  const total = amount.reduce((acc: number, item: number) => (acc += item), 0);
+  console.log(total);
+
+  return (
+    <div className="balancecomponent">
+      <h2>Current Balance</h2>
+
+      <h3>
+        <b>${total}</b>
+      </h3>
+    </div>
+  );
+};
+
+export default Balance;
