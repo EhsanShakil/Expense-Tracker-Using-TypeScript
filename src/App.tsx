96eae8ecab558d3e2290a8447a3983d99c64102c
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-09-01 23:31:46 +0500

    Updating Expense Component

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,15 @@ import AddTransaction from "./Components/Add_Transaction";
 import TransactionHistory from "./Components/Transaction_History";
 import Balance from "./Components/Balance";
 import Income from "./Components/Income";
+import Expense from "./Components/Expense";
+
 function App() {
   return (
     <GlobalProvider>
       <h1>Expense Tracker</h1>
       <Balance />
       <Income />
+      <Expense />
       <AddTransaction />
       <TransactionHistory />
     </GlobalProvider>
--- src/Components/Expense.tsx
+++ src/Components/Expense.tsx
@@ -1,1 +1,25 @@
-import react from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../GlobalState/Gloabal_State";
+
+const Balance = () => {
+  const { transaction }: any = useContext(GlobalContext);
+
+  const amount = transaction.filter(
+    (transaction: any) => transaction.Amount < 0
+  );
+  const income = amount.map((income: any) => income.Amount);
+  const total = income.reduce((acc: number, item: number) => (acc += item), 0);
+  console.log(total);
+
+  return (
+    <div className="balancecomponent">
+      <h2>Income</h2>
+
+      <h3>
+        <b>-${Math.abs(total)}</b>
+      </h3>
+    </div>
+  );
+};
+
+export default Balance;
