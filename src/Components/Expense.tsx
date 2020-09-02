a503a5dcb39fd4c80e74b375cefe7ac8dcf2f184
Ahsan Shakeel <ahsanshakeel1994@gmail.com>
2020-09-02 14:22:44 +0500

    Update Expense.tsx

--- src/Components/Expense.tsx
+++ src/Components/Expense.tsx
@@ -7,13 +7,13 @@ const Balance = () => {
   const amount = transaction.filter(
     (transaction: any) => transaction.Amount < 0
   );
-  const income = amount.map((income: any) => income.Amount);
-  const total = income.reduce((acc: number, item: number) => (acc += item), 0);
+  const expense = amount.map((income: any) => income.Amount);
+  const total = expense.reduce((acc: number, item: number) => (acc += item), 0);
   console.log(total);
 
   return (
     <div className="balancecomponent">
-      <h2>Income</h2>
+      <h2>Expense</h2>
 
       <h3>
         <b>-${Math.abs(total)}</b>
